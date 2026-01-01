6cbc528a5cf8ac9f372b6aef291dade4a894af60
Anuanwarbaltee <anwardines786@gmail.com>
2026-01-02 01:08:59 +0500

    update cors origin code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ import cors from 'cors'
 
 const app = express();
 
-app.use(cors({origin:`${process.env.COR_ORIGIN}`,credentials:true}));
+// app.use(cors({origin:`${process.env.COR_ORIGIN}`,credentials:true}));
+app.use(cors({
+  origin: process.env.COR_ORIGIN.split(","),
+  credentials: true
+}));
+
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"))

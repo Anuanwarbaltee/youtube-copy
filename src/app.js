1ee441d45ffeeeffdf28fd199bd5f459198ceab4
Anuanwarbaltee <anwardines786@gmail.com>
2026-01-02 21:41:22 +0500

    reset origin code

--- src/app.js
+++ src/app.js
@@ -5,11 +5,11 @@ import cors from 'cors'
 
 const app = express();
 
-// app.use(cors({origin:`${process.env.COR_ORIGIN}`,credentials:true}));
-app.use(cors({
-  origin: process.env.COR_ORIGIN.split(","),
-  credentials: true
-}));
+app.use(cors({origin:`${process.env.COR_ORIGIN}`,credentials:true}));
+// app.use(cors({
+//   origin: process.env.COR_ORIGIN.split(","),
+//   credentials: true
+// }));
 
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));

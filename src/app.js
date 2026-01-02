37fb2c4543cb23ae151e235e5a02b3503374bc4e
Anuanwarbaltee <anwardines786@gmail.com>
2026-01-03 00:19:41 +0500

    set core origin for multiple urls

--- src/app.js
+++ src/app.js
@@ -5,11 +5,20 @@ import cors from 'cors'
 
 const app = express();
 
-app.use(cors({origin:`${process.env.COR_ORIGIN}`,credentials:true}));
-// app.use(cors({
-//   origin: process.env.COR_ORIGIN.split(","),
-//   credentials: true
-// }));
+// app.use(cors({origin:`${process.env.COR_ORIGIN}`,credentials:true}));
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true); 
+    if(!allowedOrigins.includes(origin)){
+      return callback(new Error('CORS policy blocked this origin'), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
